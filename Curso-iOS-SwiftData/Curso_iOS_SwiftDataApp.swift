5b14c5fad4793c5e049d16c88fb5233aa7cf7d84
Eduardo <eduardo@microsoft.com>
2026-02-17 12:12:32 +0100

    Initial Commit

---
+++ Curso-iOS-SwiftData/Curso_iOS_SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Curso_iOS_SwiftDataApp.swift
+//  Curso-iOS-SwiftData
+//
+//  Created by Equipo 2 on 17/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Curso_iOS_SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
